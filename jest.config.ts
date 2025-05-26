a005cc73a5372f5a4ac85f381274ad6096c425c6
Dylan Applegate <dean.brophy@gmail.com>
2025-05-26 11:24:53 -0600

    Update jest.config.ts to include koans directory in testPathIgnorePatterns

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
+  testPathIgnorePatterns: ['/src/koans/'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
